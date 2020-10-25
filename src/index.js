ae7dd36bc0be272ca13e0858ee6d8385c4f3ee22
Gleb Pobezhimov <pglb@yandex.ru>
2020-10-25 16:52:37 +0400

    feat: add partial solution for brackets task;

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+   
+  if (str.length <= 1) {
+    return false;
+  }    
+
+  let matchingOpeningBracket, ch
+  let stack = []
+
+  const openingBrackets = [];
+  const closingBrackets = [];
+
+  bracketsConfig.forEach((item) => {
+    openingBrackets.push(item[0]);
+    closingBrackets.push(item[1]);
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    ch = str[i]
+
+    if (closingBrackets.indexOf(ch) > -1) {
+      matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)]
+      if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) {
+        return false
+      }
+    } else {
+      stack.push(ch)
+    }
+  }
+
+  return (stack.length == 0)
+};
